864b53f34a8f0ff9cd6a4241837b3ba2c48bf9f4
Wiktor Plaga <wiktor.plaga@tzif.io>
2024-06-27 17:22:52 +0200

    ADD packagist.org attempt

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/safe-utils/.github
+
+go 1.22.4
---
+++ safeutils.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// SafeUtils returns a greeting for the named person.
+func main() {
+	fmt.Println("SafeUtils")
+}
